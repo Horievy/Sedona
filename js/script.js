29aa973d7d68093c203da77022cb4027af830b7c
Oleksandr Horievy <alexandr.gorevoy07@gmail.com>
2018-03-13 15:33:40 +0200

    Sedona finised

--- js/script.js
+++ js/script.js
@@ -10,20 +10,20 @@
 			popup.classList.toggle("form-booking-show");
 			popup.classList.remove("form-booking-error");
 			checkin.focus();
-			if(storage){
-				checkin.value = storage;
-				checkout.focus();
-			}else {
-				checkin.focus();
-			}
+			// if(storage){
+			// 	checkin.value = storage;
+			// 	checkout.focus();
+			// }else {
+			// 	checkin.focus();
+			// }
 		});
 
-		popup.addEventListener('submit', function (event){
-			if(!checkin.value || !checkout.value){
-			event.preventDefault();
-			popup.classList.add("form-booking-error");
-			console.log("введи что то");
-			 }else {
-				localStorage.setItem('checkin', "27 Августа");
-			}
-		});
+		// popup.addEventListener('submit', function (event){
+		// 	if(!checkin.value || !checkout.value){
+		// 	event.preventDefault();
+		// 	popup.classList.add("form-booking-error");
+		// 	console.log("введи что то");
+		// 	 }else {
+		// 		localStorage.setItem('checkin', "27 Августа");
+		// 	}
+		// });
